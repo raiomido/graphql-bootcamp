382a22579710ac0cb836b5e63b62df2c05828a10
Rai Omido <raiomido@gmail.com>
2023-03-04 13:01:54 +0300

    Initial commit

---
+++ server/src/resolvers.js
@@ -0,0 +1,13 @@
+const resolvers = {
+    Query: {
+        /**
+         * Returns an array of tracks that will be used to populate
+         * the homepage grid of our web client
+         */
+        tracksForHome: () => {
+
+        }
+    }
+};
+
+module.exports = resolvers;
